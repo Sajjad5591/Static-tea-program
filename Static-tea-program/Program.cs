0867297a45b741629a9fe4ad53b47dca2b8d4e76
Sajjad5591 <sajjadirfan5591@gmail.com>
2025-04-20 21:53:09 -0700

    Add project files.

---
+++ Static-tea-program/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace consoleeApplication
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            tea(); 
+        }
+
+        static void tea()
+        {
+            Console.WriteLine("boil water");
+            Console.WriteLine("take a ");
+            Console.WriteLine("take a tea");
+            Console.WriteLine("boil it");
+            Console.WriteLine("make what you want");
+            Console.WriteLine("Drink your tea.");
+        }
+    }
+}
